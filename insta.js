c9c5f22e1d2ba0edf2d655a6537f85e4318f981a
chaptercalc <dutcher.reid@gmail.com>
2016-06-29 21:09:43 -0700

    push

--- insta.js
+++ insta.js
@@ -1,22 +1,56 @@
-var token = 'your access token', // learn how to obtain it below
-    userid = 1362124742, // User ID - get it in source HTML of your Instagram profile or look at the next example :)
-    num_photos = 4; // how much photos do you want to get
- 
-$.ajax({
-	url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
-	dataType: 'jsonp',
-	type: 'GET',
-	data: {access_token: token, count: num_photos},
-	success: function(data){
- 		console.log(data);
-		for( x in data.data ){
-			$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
-			// data.data[x].images.thumbnail.url - URL of image 150х150
-			// data.data[x].images.standard_resolution.url - URL of image 612х612
-			// data.data[x].link - Instagram post URL 
+$(document).ready(function() {
+
+var token = '269761860.af46082.4db827bfedc6424b868f37df74661f4f'
+var userid = 269761860 // User ID - get it in source HTML of your Instagram profile or look at the next example :)
+var num_photos = 4; // how much photos do you want to get
+
+$('#All').click(function(event) {
+    // prevents page refresh
+    event.preventDefault();
+	$.ajax({
+		url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
+		dataType: 'jsonp',
+		type: 'GET',
+		data: {access_token: token, count: num_photos},
+		success: function(data){
+	 		console.log(data);
+				for(var i = 0; i < num_photos; i++){
+				//console.log(data.data[i].images.low_resolution.url)
+
+				$('body').append('<img src="'+data.data[i].images.low_resolution.url+'">'); 
+				// data.data[x].images.low_resolution.url - URL of image, 306х306
+				// data.data[x].images.thumbnail.url - URL of image 150х150
+				// data.data[x].images.standard_resolution.url - URL of image 612х612
+				// data.data[x].link - Instagram post URL 
+				};
 		}
-	},
-	error: function(data){
-		console.log(data); // send the error notifications to console
-	}
+	});
+});
+
+
+$('#Search').submit(function(event) {
+    // prevents page refresh
+    event.preventDefault();
+    var hashtag = $('#instaTag').val();
+
+    console.log(hashtag);
+    num_photos = 4;
+ 
+		$.ajax({
+			url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent',
+			dataType: 'jsonp',
+			type: 'GET',
+			data: {access_token: token, count: num_photos},
+			success: function(data){
+				console.log(data);
+				for(var i = 0; i < num_photos; i++){
+					$('body').append('<img src="'+data.data[i].images.low_resolution.url+'">'); 
+				}
+			},
+			error: function(data){
+				console.log(data);
+			}
+		});
+
+	});
 });
--- main.js
+++ main.js
@@ -11,69 +11,138 @@ $(document).ready(function() {
     event.preventDefault();
 
     // grab user input
-    var message = $('#InsuranceCompany').val();
-    var message2 = $('#MP').val();
-    var message3 = $('#LinesOfBusiness').val();
-    var message4 = $('#Underwriter').val();
-    var message5 = $('#hitList').val();
+    var InsuranceCompany = $('#InsuranceCompany').val();
+    var MP = $('#MP').val();
+    var LinesOfBusiness = $('#LinesOfBusiness').val();
+    var Underwriter = $('#Underwriter').val();
+    var HitList = $('#hitList').val();
 
     var messagesReference = myDBReference.child('messages');
 
     messagesReference.push({
-      message: {
-        Company: message,
-        MP: message2,
-        LinesOfBusiness: message3,
-        Underwriter: message4,
-        HitList: message5
-      }
-
-
+        Company: InsuranceCompany,
+        MP: MP,
+        LinesOfBusiness: LinesOfBusiness,
+        Underwriter: Underwriter,
+        HitList: HitList
     });
-
-    // messagesReference.push({
-    //   message: message + " | " + message2 + " | " + message3 + " | " + message4 + " | " + message5
-
-    // });
 
 
   });
 
-  myDBReference.child('messages').on('child_added', function(results) { // read docs on child_added
-    results.forEach(function(message) {
 
-      var data = {
-        message: message.val() //How do I call disply the object?
-      };
+$('#showAllMarkets').click(function(event){
+  
+  myDBReference.child('messages').on('child_added', function(results) { 
+      
+        var data = {
+          Company: results.val().Company,
+          MP: results.val().MP,
+          LinesOfBusiness: results.val().LinesOfBusiness,
+          Underwriter: results.val().Underwriter,
+          HitList: results.val().HitList,
+        };
 
-      var templateHTML = template(data);
+        var templateHTML = template(data);
+        $('#messages-list').append(templateHTML);
 
-      $('#messages-list').append(templateHTML);
     });
-  });
 
-// show all items in the data base, when called. 
-// $('#showAll').click(function(event){
-//   event.preventDefault();
-//   myDBReference.on("value", function(message) {
+});
 
-//     var data = {
-//       message: message.val()
-//     };
 
-//     var templateHTML = template(data);
-//     $('#messages-list').append(templateHTML);
+//query for only ENVmarkets
+$('#envMarkets').click(function(event){
+   //console.log(myDBReference.child('messages').ref().orderByChild('Company'));
+      myDBReference.child('messages').ref().orderByChild('Company').equalTo('Auto').on('child_added', function(results) { 
+
+              var data = {
+                Company: results.val().Company,
+                MP: results.val().MP,
+                LinesOfBusiness: results.val().LinesOfBusiness,
+                Underwriter: results.val().Underwriter,
+                HitList: results.val().HitList,
+              };
+
+              var templateHTML = template(data);
+              $('#messages-list').append(templateHTML);
+
+      });
+});
+
+
+//$('#construction').click(function(event){
+  
+  myDBReference.child('messages').on('child_added', function(results) { 
+      
+
+        var data = {
+          Company: results.val().Company,
+          MP: results.val().MP,
+          LinesOfBusiness: results.val().LinesOfBusiness,
+          Underwriter: results.val().Underwriter,
+          HitList: results.val().HitList,
+        };
+
+        var templateHTML = template(data);
+        $('#messages-list').append(templateHTML);
+
+    });
+
+//});
+
+$('#envMarkets').click(function(event){
+   //console.log(myDBReference.child('messages').ref().orderByChild('Company'));
+      myDBReference.child('messages').ref().orderByChild('Company').equalTo('Auto').on('child_added', function(results) { 
+
+              var data = {
+                Company: results.val().Company,
+                MP: results.val().MP,
+                LinesOfBusiness: results.val().LinesOfBusiness,
+                Underwriter: results.val().Underwriter,
+                HitList: results.val().HitList,
+              };
+
+              var templateHTML = template(data);
+              $('#messages-list').append(templateHTML);
+
+      });
+});
+
+
+
+
+
+
+//       var templateHTML = template(data);
+// ​
+//       var $templateHTML = $(templateHTML);
+// ​
+//       $templateHTML.click(function() {
+//         var messageId = $(this).data('id');
+//         updateMessage(messageId);
+//       })
+//       $('#messages-list').append($templateHTML);
+//     });
 //   });
 
-// });
+
+//   // Delete functionality
+//   function deleteMessage(id) {
+//     var messageReference = new Firebase('https://fan-message-board.firebaseio.com/messages/' + id);
+// ​
+//     messageReference.remove();
+//   }
 
 
-
-
+// $('#messages-list').click(function(event){
 
 // var messagesReference = new Firebase('https://js-finalproject.firebaseio.com/messages/')
 
 // messagesReference.remove();
 
+// });
+
 
 });
+
